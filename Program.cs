0b6f4be6dbc4e9b9d23eb1db630ae35e1dc72e76
MotieeAtefe <motiee1377@gmaile.com>
2024-10-10 17:20:34 +0330

    impleament TrafficLightController with enum structure

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace enumeration
+﻿using static enumeration.TrafficLightController;
+
+namespace enumeration
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            TrafficLightController trafficLightController = new TrafficLightController();
+            trafficLightController.GetLightDescription();   
+
+            trafficLightController.CurrentLight = TrafficLight.green;
+            trafficLightController.GetLightDescription();
+
+            trafficLightController.CurrentLight = TrafficLight.yellow;
+            trafficLightController.GetLightDescription();
         }
     }
 }
---
+++ TrafficLightController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Timers;
+
+namespace enumeration
+{
+    internal class TrafficLightController
+    {
+      
+        public enum TrafficLight
+        {
+            red, green, yellow
+        }
+        public TrafficLight CurrentLight { get; set; }
+        public TrafficLightController()
+        {
+            CurrentLight = TrafficLight.red;
+            
+        }
+        public void GetLightDescription()
+        {
+            switch (CurrentLight)
+            {
+                case TrafficLight.red:
+                    Console.WriteLine("Stop! The light is red.");
+                    break;
+                case TrafficLight.yellow:
+                    Console.WriteLine("Caution! The light is yellow.");
+                    break;
+                case TrafficLight.green:
+                    Console.WriteLine("Go! The light is green.");
+                    break;
+                default:
+                    Console.WriteLine("Unknown light state.");
+                    break;
+            }
+        }
+
+      
+        
+
+
+
+
+
+    }
+}
